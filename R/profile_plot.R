f164af04e7462c6f3db5f08b6749c0eeec93f4ef
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-04-05 17:07:03 -0400

    added notes on figure to prodcue

--- R/profile_plot.R
+++ R/profile_plot.R
@@ -6,3 +6,9 @@ profiles %>%
   ggplot(aes(x = prop_distance, y = elevation, color = factor(year))) +
   geom_line() +
   facet_grid(transect ~ ., scales = "free")
+
+# Build out figure with dist (or proportional dist) on x and elevation on y
+# Show different habitat sections of profiles
+# maybe break out habitats into separate plots
+# Do per transect
+# Smooth profiles (neg exponential used prior)
